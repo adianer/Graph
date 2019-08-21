e61a10eb35b35faab931ede7bfcb3bbd3a7082cd
adianer <962622577@qq.com>
2019-08-21 19:30:57 +0800

    8月21日作业

---
+++ src/Circle.java
@@ -0,0 +1,6 @@
+public class Circle extends Graph {
+    public  void  area(){
+        System.out.println("请输入半径");
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle extends Graph {
+    public  void  area(){
+        System.out.println("请输入底长和高");
+    }
+
+}
